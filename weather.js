983b57202639409b702c54048d91d1301b6a1646
Jose Moreno <morenojr90@yahoo.com>
2018-06-20 09:50:00 -0700

    Initial setup1

---
+++ weather.js
@@ -0,0 +1,20 @@
+const https = require('https');
+const api = require('./api.json');
+
+//Print out temp details
+//Print out error message
+
+function get(query) {
+  const request = https.get(`https://api.wunderground.com/api/${api.key}/geolookup/conditions/q/${query}.json`), response => {
+    let body = "";
+    //Read the data
+    response.on('data', chunk => {
+      body += chunk;
+    })
+    response.on('end', () => {
+      console.log(body);
+      //Parse data
+      //Print the data
+    })
+  }
+}
